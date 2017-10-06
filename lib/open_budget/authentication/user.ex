57bf810f58d33e0575d98f164423b35791c8cc86
Terence Ponce <terenceponce@gmail.com>
2017-10-06 08:54:23 +0800

    Fix credo errors for User

--- lib/open_budget/authentication/user.ex
+++ lib/open_budget/authentication/user.ex
@@ -1,8 +1,15 @@
 defmodule OpenBudget.Authentication.User do
+  @moduledoc """
+  User schema
+
+  Representation of our users in the system.
+  This is where we associate almost all of the
+  data we have surrounding budgeting.
+  """
+
   use Ecto.Schema
   import Ecto.Changeset
   alias OpenBudget.Authentication.User
-
 
   schema "users" do
     field :email, :string
