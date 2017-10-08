74ee6abc99ce1360239b4e1ef61aba948ee66c2d
Terence Ponce <terenceponce@gmail.com>
2017-10-08 21:40:13 +0800

    Add 404 handler for Token#create and add tests for it

--- lib/open_budget_web/controllers/token_controller.ex
+++ lib/open_budget_web/controllers/token_controller.ex
@@ -13,12 +13,17 @@ defmodule OpenBudgetWeb.TokenController do
         token = Plug.current_token(conn)
 
         conn
+        |> put_status(201)
         |> put_resp_header("authorization", "Bearer #{token}")
         |> json(%{access_token: token})
       {:error, :unauthorized, conn} ->
         conn
         |> put_status(401)
         |> render(OpenBudgetWeb.ErrorView, "401.json-api")
+      {:error, :not_found, conn} ->
+        conn
+        |> put_status(404)
+        |> render(OpenBudgetWeb.ErrorView, "404.json-api")
     end
   end
 
