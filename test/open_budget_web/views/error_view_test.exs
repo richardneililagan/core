fd9516722fd442c9f45d077da88a38309f2863b6
Terence Ponce <terenceponce@gmail.com>
2017-10-08 09:28:11 +0800

    Convert error views into json-api

--- test/open_budget_web/views/error_view_test.exs
+++ test/open_budget_web/views/error_view_test.exs
@@ -4,18 +4,51 @@ defmodule OpenBudgetWeb.ErrorViewTest do
   # Bring render/3 and render_to_string/3 for testing custom views
   import Phoenix.View
 
-  test "renders 404.json" do
-    assert render(OpenBudgetWeb.ErrorView, "404.json", []) ==
-           %{errors: %{detail: "Page not found"}}
+  test "renders 401.json-api" do
+    assert render(OpenBudgetWeb.ErrorView, "401.json-api", []) ==
+           %{
+             "errors" => [%{code: 401, title: "Unauthorized"}],
+             "jsonapi" => %{"version" => "1.0"}
+            }
   end
 
-  test "render 500.json" do
-    assert render(OpenBudgetWeb.ErrorView, "500.json", []) ==
-           %{errors: %{detail: "Internal server error"}}
+  test "renders 403.json-api" do
+    assert render(OpenBudgetWeb.ErrorView, "403.json-api", []) ==
+           %{
+             "errors" => [%{code: 403, title: "Forbidden"}],
+             "jsonapi" => %{"version" => "1.0"}
+            }
+  end
+
+  test "renders 404.json-api" do
+    assert render(OpenBudgetWeb.ErrorView, "404.json-api", []) ==
+           %{
+             "errors" => [%{code: 404, title: "Resource not found"}],
+             "jsonapi" => %{"version" => "1.0"}
+            }
+  end
+
+  test "renders 422.json-api" do
+    assert render(OpenBudgetWeb.ErrorView, "422.json-api", []) ==
+           %{
+             "errors" => [%{code: 422, title: "Unprocessable entity"}],
+             "jsonapi" => %{"version" => "1.0"}
+            }
+  end
+
+  test "render 500.json-api" do
+    assert render(OpenBudgetWeb.ErrorView, "500.json-api", []) ==
+           %{
+             "errors" => [%{code: 500, title: "Internal server error"}],
+             "jsonapi" => %{"version" => "1.0"}
+            }
   end
 
   test "render any other" do
     assert render(OpenBudgetWeb.ErrorView, "505.json", []) ==
-           %{errors: %{detail: "Internal server error"}}
+           %{
+             "errors" => [%{code: 500, title: "Internal server error"}],
+             "jsonapi" => %{"version" => "1.0"}
+            }
   end
 end
